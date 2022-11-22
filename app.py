4ac279ec8066612f2a1c886b684cc88f431614dc
Hamroz Gavharov <rootadmin@Hasbullas-MacBook-Pro.local>
2022-11-22 23:42:18 +0600

    server

--- app.py
+++ app.py
@@ -358,6 +358,6 @@ def name_to_figure(fig_name):
     return dcc.Graph(figure=figure)
 
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     app.run_server("0.0.0.0", debug=False, port=int(
         os.environ.get('PORT', 8000)))
