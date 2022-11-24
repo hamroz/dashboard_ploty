a821d24c532c0f20a632de83783b8478406971af
hamroz <hamroz.gavharov@gmail.com>
2022-11-25 02:27:43 +0600

    major fixes

--- app.py
+++ app.py
@@ -28,57 +28,163 @@ server = app.server
 app.title = "Dash"
 
 df = pd.read_excel(
-    "assets/dashboard.xlsx",
+    "Homework 6/homework_task/assets/dashboard.xlsx",
 )
 
 df_dt_grouped = df.groupby("Date")["Outcome"].count()
 
 
-def partA():
+def partA(outcome):
+    figure = go.Figure()
 
     totac = df.groupby("Date")["Outcome"].count()
     totsuc = df[df["Outcome"] == "Success"].groupby("Date")["Outcome"].count()
     totnsuc = df[df["Outcome"] == "Failure"].groupby("Date")["Outcome"].count()
     ratio = totac[totsuc.index]
 
-    return totac, totsuc, totnsuc, ratio
+    figure.add_trace(trace=go.Line(
+        x=totac.index, y=totac.values, name="Total calls"))
+    figure.add_trace(trace=go.Line(
+        x=totsuc.index, y=totsuc.values, name="Success"))
+    figure.add_trace(trace=go.Line(
+        x=totnsuc.index, y=totnsuc.values, name="Non Success"))
+
+    if outcome == "Success":
+        pass
+    else:
+        figure.add_trace(
+            trace=go.Line(
+                x=totsuc.index, y=totsuc.values * 100 / ratio, name="Ration success/total calls"
+            )
+        )
+
+    figure["layout"][
+        "title"
+    ] = "We want to see this data in a graph with a time series legend. Then we want to see in the same graph the ratio of success /total calls as a function of date."
+    figure["layout"]["xaxis"]["title"] = "Date"
+    figure["layout"]["yaxis"]["title"] = "Number of calls or ratio"
+    figure["layout"]["legend_title"] = "Time series"
+
+    return figure
 
 
 def partB():
+    figure = go.Figure()
 
-    totsuc = df[df["Outcome"] == "Success"].groupby("State")["Outcome"].count()
-    totnsuc = df[df["Outcome"] == "Failure"].groupby("State")[
+    a = df[df["Outcome"] == "Success"].groupby("State")["Outcome"].count()
+    b = df[df["Outcome"] == "Failure"].groupby("State")[
         "Outcome"].count()
 
-    return totsuc, totnsuc
+    # a, b = partB()
+    figure.add_trace(
+        trace=go.Bar(
+            x=a.index,
+            y=a.values,
+            name="Success",
+        )
+    )
+    figure.add_trace(
+        trace=go.Bar(
+            x=b.index,
+            y=b.values,
+            name="Failure",
+        )
+    )
+
+    figure["layout"][
+        "title"
+    ] = "We want to see another graph that presents the success and failure by State in the form of a bar graph."
+    figure["layout"]["xaxis"]["title"] = "Date"
+    figure["layout"]["yaxis"]["title"] = "Number of calls"
+    figure["layout"]["legend_title"] = "Time series"
+
+    return figure
 
 
 def partC():
-
+    figure = go.Figure()
     success_failed = df.groupby("Outcome")["Outcome"].count()
 
-    return success_failed
+    figure.add_trace(
+        trace=go.Pie(
+            labels=success_failed.index,
+            values=success_failed.values,
+        ),
+    )
+
+    figure["layout"][
+        "title"
+    ] = "We want to see a piechart that displays failure-success-timeout as a percentage"
+    figure["layout"]["legend_title"] = "Labels"
+
+    return figure
 
 
 def partD():
+    figure = go.Figure()
 
     totac = df.groupby("State")["Outcome"].count()
     totsuc = df[df["Outcome"] == "Success"].groupby("State")["Outcome"].count()
 
     state_success = (totsuc / totac * 100).sort_values(ascending=False)
 
-    return state_success
+    figure.add_trace(
+        go.Bar(
+            x=state_success.index,
+            y=state_success.values,
+            name="State success",
+        )
+    )
+
+    figure["layout"][
+        "title"
+    ] = "We want to see at the end which state was the most ' successful ' in share ratios."
+    figure["layout"]["xaxis"]["title"] = "State"
+    figure["layout"]["yaxis"]["title"] = "Success"
+    figure["layout"]["legend_title"] = "Legends"
+
+    return figure
 
 
 def partE():
+    figure = go.Figure()
 
     totac = df.groupby("State")["Outcome"].count()
     totsuc = df[df["Outcome"] == "Success"].groupby("State")["Outcome"].count()
 
-    return totac, totsuc
+    figure.add_trace(
+        go.Pie(
+            labels=totac.index,
+            values=totac.values,
+            textinfo="none",
+            name="total calls",
+            hole=0.6,
+        ),
+    )
+
+    figure.add_trace(
+        go.Pie(
+            labels=totsuc.index,
+            values=totsuc.values,
+            textinfo="none",
+            name="success calls",
+            hole=0.45,
+        ),
+    )
+    figure.data[0].domain = {"x": [0, 1], "y": [1, 1]}
+    figure.data[1].domain = {"x": [0, 1], "y": [0.22, 0.78]}
+    figure.update_traces(hoverinfo="label+percent+name")
+
+    figure["layout"][
+        "title"
+    ] = "We also want to see a double piechart that displays the total number of actions/ State and number of success / state ."
+    figure["layout"]["legend_title"] = "Labels"
+
+    return figure
 
 
 def partF():
+    figure = go.Figure()
 
     df["Success"] = df["Outcome"].apply(
         lambda outcome: 1 if outcome == "Success" else 0
@@ -92,14 +198,29 @@ def partF():
         else time_period
     )
 
-    return time_period.groupby("Time_Period")["Success"].sum()
+    x = time_period.groupby("Time_Period")["Success"].sum()
+    figure.add_trace(
+        go.Bar(
+            x=x.index,
+            y=x.values,
+            name="Time Period",
+        )
+    )
+
+    figure["layout"][
+        "title"
+    ] = "We want to know the number of succes by Time_Period (be careful with the ordering)"
+    figure["layout"]["xaxis"]["title"] = "Hours/Time"
+    figure["layout"]["yaxis"]["title"] = "Success calls"
+
+    return figure
 
 
 def filterDate(state, outcome, startDate, endDate):
     global df, df_dt_grouped
 
     df = pd.read_excel(
-        "assets/dashboard.xlsx",
+        "Homework 6/homework_task/assets/dashboard.xlsx",
     )
 
     if state == "All" and outcome == "All":
@@ -110,7 +231,6 @@ def filterDate(state, outcome, startDate, endDate):
     elif outcome == "All":
         df = df[(df.Date >= startDate) & (df.Date <= endDate)]
         df = df[df.State.isin(state)]
-
     else:
         df = df[(df.Date >= startDate) & (df.Date <= endDate)]
         df = df[df.State.isin(state)]
@@ -127,13 +247,15 @@ fig_dropdown = html.Div(
 
         dcc.Dropdown(
             id="fig_dropdown",
+            multi=True,
             options=[{"label": x, "value": x} for x in fig_names],
-            value="A",
+            value="All",
+            placeholder="All",
         )
     ],
 )
 
-fig_plot = html.Div(id="fig_plot", style={"margin": "20px"})
+fig_plot = html.Div(id="fig_plot", style={"margin": "20px"},)
 
 app.layout = html.Div(
     [
@@ -159,9 +281,7 @@ app.layout = html.Div(
                         html.Div(children="Outcome", className="menu-title"),
                         dcc.Dropdown(
                             id="outcome-filter",
-                            options=["All", "Success", "Failure",
-
-                                     ],
+                            options=["All", "Success", "Failure"],
                             value="All",
                             clearable=False,
                             className="dropdown",
@@ -209,6 +329,7 @@ app.layout = html.Div(
 @app.callback(
     Output("fig_plot", "children"),
 
+
     [Input("fig_dropdown", "value"),
      Input("outcome-filter", "value"),
      Input("state-filter", "value"),
@@ -217,146 +338,32 @@ app.layout = html.Div(
      ]
 )
 def update_output(fig_name, outcome, state, startDate, endDate):
-    print(outcome)
-    print(type(state))
-    print(startDate)
-    print(endDate)
+
+    parts = {"A": partA(outcome), "B": partB(), "C": partC(),
+             "D": partD(), "E": partE(), "F": partF()}
 
     if state == None or len(state) == 0:
         state = "All"
 
+    if fig_name == None or len(fig_name) == 0:
+        fig_name = "All"
+
     filterDate(state, outcome, startDate, endDate)
 
-    return name_to_figure(fig_name)
-
-
-def name_to_figure(fig_name):
-    figure = go.Figure()
-
-    if fig_name == "A":
-        a, b, c, d = partA()
-        figure.add_trace(trace=go.Line(
-            x=a.index, y=a.values, name="Total calls"))
-        figure.add_trace(trace=go.Line(x=b.index, y=b.values, name="Success"))
-        figure.add_trace(trace=go.Line(
-            x=c.index, y=c.values, name="Non Success"))
-
-        figure.add_trace(
-            trace=go.Line(
-                x=b.index, y=b.values * 100 / d, name="Ration success/total calls"
-            )
+    if fig_name == "All":
+        return html.Div(
+            [
+                dcc.Graph(figure=partA(outcome)),
+                dcc.Graph(figure=partB()),
+                dcc.Graph(figure=partC()),
+                dcc.Graph(figure=partD()),
+                dcc.Graph(figure=partE()),
+                dcc.Graph(figure=partF())
+            ])
+    else:
+        return html.Div(
+            children=[dcc.Graph(figure=parts[i]) for i in fig_name]
         )
-
-        figure["layout"][
-            "title"
-        ] = "We want to see this data in a graph with a time series legend. Then we want to see in the same graph the ratio of success /total calls as a function of date."
-        figure["layout"]["xaxis"]["title"] = "Date"
-        figure["layout"]["yaxis"]["title"] = "Number of calls or ratio"
-        figure["layout"]["legend_title"] = "Time series"
-
-    elif fig_name == "B":
-        a, b = partB()
-        figure.add_trace(
-            trace=go.Bar(
-                x=a.index,
-                y=a.values,
-                name="Success",
-            )
-        )
-        figure.add_trace(
-            trace=go.Bar(
-                x=b.index,
-                y=b.values,
-                name="Failure",
-            )
-        )
-
-        figure["layout"][
-            "title"
-        ] = "We want to see another graph that presents the success and failure by State in the form of a bar graph."
-        figure["layout"]["xaxis"]["title"] = "Date"
-        figure["layout"]["yaxis"]["title"] = "Number of calls"
-        figure["layout"]["legend_title"] = "Time series"
-
-    elif fig_name == "C":
-        success_failed = partC()
-        figure.add_trace(
-            trace=go.Pie(
-                labels=success_failed.index,
-                values=success_failed.values,
-            ),
-        )
-
-        figure["layout"][
-            "title"
-        ] = "We want to see a piechart that displays failure-success-timeout as a percentage"
-        figure["layout"]["legend_title"] = "Labels"
-
-    elif fig_name == "D":
-        a = partD()
-        figure.add_trace(
-            go.Bar(
-                x=a.index,
-                y=a.values,
-                name="State success",
-            )
-        )
-
-        figure["layout"][
-            "title"
-        ] = "We want to see at the end which state was the most ' successful ' in share ratios."
-        figure["layout"]["xaxis"]["title"] = "State"
-        figure["layout"]["yaxis"]["title"] = "Success"
-        figure["layout"]["legend_title"] = "Legends"
-
-    elif fig_name == "E":
-        a, b = partE()
-
-        figure.add_trace(
-            go.Pie(
-                labels=a.index,
-                values=a.values,
-                textinfo="none",
-                name="total calls",
-                hole=0.6,
-            ),
-        )
-
-        figure.add_trace(
-            go.Pie(
-                labels=b.index,
-                values=b.values,
-                textinfo="none",
-                name="success calls",
-                hole=0.45,
-            ),
-        )
-        figure.data[0].domain = {"x": [0, 1], "y": [1, 1]}
-        figure.data[1].domain = {"x": [0, 1], "y": [0.22, 0.78]}
-        figure.update_traces(hoverinfo="label+percent+name")
-
-        figure["layout"][
-            "title"
-        ] = "We also want to see a double piechart that displays the total number of actions/ State and number of success / state ."
-        figure["layout"]["legend_title"] = "Labels"
-
-    elif fig_name == "F":
-        x = partF()
-        figure.add_trace(
-            go.Bar(
-                x=x.index,
-                y=x.values,
-                name="Time Period",
-            )
-        )
-
-        figure["layout"][
-            "title"
-        ] = "We want to know the number of succes by Time_Period (be careful with the ordering)"
-        figure["layout"]["xaxis"]["title"] = "Hours/Time"
-        figure["layout"]["yaxis"]["title"] = "Success calls"
-
-    return dcc.Graph(figure=figure)
 
 
 if __name__ == "__main__":
